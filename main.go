15de5d814169f1531c6f3e283ba65d1e7b7e4dc8
Jon Wade <jonwadeuk@gmail.com>
2021-06-14 09:48:44 +0100

    complete site parsing logic

--- main.go
+++ main.go
@@ -5,25 +5,48 @@ import (
 	"golang.org/x/net/html"
 	"net/http"
 	"os"
+	"strings"
+	"sync"
 )
 
-func traverse(n *html.Node) {
-	//fmt.Printf("%+v\n", n)
-	//fmt.Println("type", n.Type)
-	//fmt.Println("attr", n.Attr)
-	//fmt.Println("data", n.Data)
-	//fmt.Println("atom", n.DataAtom)
-	//fmt.Println("")
-	if n.Type == html.ElementNode && n.Data == "link" {
-		// Do something with n...
-		fmt.Println(n.Attr)
+// TODO: this function searches a slice to find a string value, to check for duplicates, inefficient, use a map to do this...
+func sliceContains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
 	}
+
+	return false
+}
+
+func traverse(url string, n *html.Node, resultSlice *[]string, mutex *sync.Mutex) {
+	// this identifies anchor type html elements...
+	if n.Type == html.ElementNode && n.Data == "a" {
+		// ...and extracts their href values
+		var urlStr = n.Attr[0].Val
+		if strings.Contains(urlStr, "http") && !strings.Contains(urlStr, "mailto:") && strings.Contains(urlStr, url) {
+			fmt.Println(urlStr)
+			// we only want to write to the slice if it doesn't already contain this value, so we don't end up in an infinite loop
+			if !sliceContains(*resultSlice, urlStr) {
+				mutex.Lock()
+				*resultSlice = append(*resultSlice, urlStr)
+				mutex.Unlock()
+
+				// then we recursively kick off the crawl again with the new page value
+				parsePage(urlStr, resultSlice, mutex)
+			}
+		}
+	}
+
+	// this allows us to traverse all the child elements of the current page element, recursively
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		traverse(c)
+		traverse(url, c, resultSlice, mutex)
 	}
 }
 
-func parsePage(url string) {
+func parsePage(url string, resultSlice *[]string, mutex *sync.Mutex) {
+	// this function parses each page's html to look for the anchor links
 	res, err := http.Get(url)
 	if err != nil {
 		panic(err)
@@ -36,81 +59,12 @@ func parsePage(url string) {
 	}()
 
 	doc, err := html.Parse(res.Body)
-	traverse(doc)
+	traverse(url, doc, resultSlice, mutex)
 }
 
 func main() {
-	parsePage("https://bbc.co.uk/news")
+	var resultSlice []string
+	var mutex = sync.Mutex{}
+	parsePage("https://thebotplatform.com", &resultSlice, &mutex)
+	fmt.Printf("\n\n%+v\n", resultSlice)
 }
-
-//func lrt(r chan int32, done chan bool) {
-//	i := 0
-//	for {
-//		i++
-//		time.Sleep(time.Second * 1)
-//		r <- rand.Int31n(100)
-//		if i == 6 {
-//			close(r)
-//			done <- true
-//			break
-//		}
-//	}
-//}
-
-//func main() {
-//	//parsePage("https://bbc.co.uk/news")
-//	r := <-longRunningTask()
-//	fmt.Println(r)
-//}
-
-//func worker(resultSlice *[]int32, wg *sync.WaitGroup, mutex *sync.Mutex) {
-//	defer wg.Done()
-//	mutex.Lock()
-//	*resultSlice = append(*resultSlice, rand.Int31n(100))
-//	mutex.Unlock()
-//}
-//
-//func main() {
-//	var resultSlice []int32
-//	var wg sync.WaitGroup
-//	var mutex = sync.Mutex{}
-//
-//	for i := 1; i <= 5; i++ {
-//		wg.Add(1)
-//		go worker(&resultSlice, &wg, &mutex)
-//	}
-//
-//	wg.Wait()
-//	fmt.Println("resultSlice", resultSlice);
-//}
-
-//func main() {
-//	resultChan := make(chan int32)
-//	done := make(chan bool)
-//	var resultSlice []int32
-//
-//	go lrt(resultChan, done)
-//
-//	for {
-//		result, more := <-resultChan
-//		if more {
-//			exists := false
-//			for _, v := range resultSlice {
-//				fmt.Println("v", v)
-//				if v == result {
-//					exists = true
-//					break
-//				}
-//			}
-//			if !exists {
-//				resultSlice = append(resultSlice, result)
-//			}
-//			fmt.Println(resultSlice)
-//			continue
-//		}
-//		fmt.Println("done")
-//		break
-//	}
-//
-//	<-done
-//}
